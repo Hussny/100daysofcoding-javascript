ef954782f0777522454eeb477bb1543186c789fc
Hussein Salat <husseinsalat24@gmail.com>
2020-07-20 22:11:16 +0300

    Added concatenate.js
    It is about adding strings

---
+++ Day_6/concatenate.js
@@ -0,0 +1,5 @@
+/*  concatenate two or more strings you use the concat() method */
+let firstName = 'Hussein';
+let fullName = firstName.concat(' ', 'Salat');
+console.log(fullName); /* "Hussein Salat" */
+console.log(firstName); /* "Hussein" */
