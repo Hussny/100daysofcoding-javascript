5955e9ed52328f0ed943e5c968527c6ba4e387e9
Hussein Salat <husseinsalat24@gmail.com>
2020-07-21 14:03:51 +0300

    Added lastindexof.js

---
+++ Day_6/lastindexof.js
@@ -0,0 +1,5 @@
+var stringone = 'I love psg, since psg is my favourite team'
+var stringtwo = 'Not the end, since end is near'
+
+console.log(stringone.lastIndexOf("psg"));
+console.log(stringtwo.lastIndexOf("end"));
