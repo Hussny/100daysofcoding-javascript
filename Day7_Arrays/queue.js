f9f888463d94c3681c9720ad7bb4275cebf6a2a1
Hussein Salat <husseinsalat24@gmail.com>
2020-07-22 06:45:02 +0300

    Added queue.js

---
+++ Day7_Arrays/queue.js
@@ -0,0 +1,14 @@
+var books = [];
+
+// enqueue
+books.push("Daring Greatly");
+books.push("Reclaim Your Heart");
+books.push("Atomic Habits");
+
+console.log(books);
+
+// dequeue
+
+books.shift();
+console.log(books);
+
