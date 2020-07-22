f710af86229849ca4c58fb36063081275ccc5f8d
Hussein Salat <husseinsalat24@gmail.com>
2020-07-22 06:35:12 +0300

    Added stack.js

---
+++ Day7_Arrays/stack.js
@@ -0,0 +1,16 @@
+var nbaplayers = [];
+
+// push
+nbaplayers.push("Curry");
+nbaplayers.push("Lebron");
+nbaplayers.push("Kyrie");
+
+console.log(nbaplayers);
+
+// pop
+nbaplayers.pop();
+console.log(nbaplayers);
+
+// peek 
+nbaplayers[nbaplayers.length -1 ];
+console.log(nbaplayers);
