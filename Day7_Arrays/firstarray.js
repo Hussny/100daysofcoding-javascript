6dd460311df1bd045172b12d1cbc885e582e1ef3
Hussein Salat <husseinsalat24@gmail.com>
2020-07-22 05:37:00 +0300

    Added first and last array

---
+++ Day7_Arrays/firstarray.js
@@ -0,0 +1,5 @@
+let name = ["Hussny", "Dashu", "Shavi", "Salat", "Alpha"];
+
+let first_name = name[2];
+
+console.log(first_name);
---
+++ Day7_Arrays/lastarray.js
@@ -0,0 +1,4 @@
+let smartphone = ["Oneplus", "Iphone", "Samsung", "Huawei"];
+let last_smartphone = smartphone[smartphone.length-1];
+
+console.log(last_smartphone);
