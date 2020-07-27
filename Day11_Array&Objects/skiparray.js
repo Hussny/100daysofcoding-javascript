2ef814e43af6bb8f6f905a57c39f4ddf315cef99
Hussein Salat <husseinsalat24@gmail.com>
2020-07-27 14:06:44 +0300

    Showed how to skip an Array

---
+++ Day11_Array&Objects/skiparray.js
@@ -0,0 +1,7 @@
+let players = ["Lebron", "Kyrie", "Luka", "Kawhi"];
+
+const [Lebron,,Kyrie] = players; // What the two comma ,, does is to skip an Array.
+
+console.log(Lebron); // Lebron
+console.log(Kyrie);     // Luka
+
