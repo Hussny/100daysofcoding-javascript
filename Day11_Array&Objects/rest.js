0a5ba4e67c393832656233d1e8b56929d2e035aa
Hussein Salat <husseinsalat24@gmail.com>
2020-07-27 20:51:36 +0300

    The use of ... in destructing array

---
+++ Day11_Array&Objects/rest.js
@@ -0,0 +1,9 @@
+const alphabet = ["A", "B", "C", "D", "E", "F"];
+
+const [a,b,c, ...rest] = alphabet;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(rest);
+
--- Day11_Array&Objects/skiparray.js
+++ Day11_Array&Objects/skiparray.js
@@ -1,6 +1,6 @@
 const alphabet=['A', 'B', 'C', 'D', 'E', 'F'];
 
-// The two comma is used to skid an array.
+// The two comma is used to skip an array.
 const[a,,c]=alphabet;
 
 console.log(a);
