36c928651103b0705ac40186a785cc69c16038ab
Hussein Salat <husseinsalat24@gmail.com>
2020-07-30 23:03:31 +0300

    Added New Ajax Files

---
+++ Day14_AJAX/app.js
@@ -0,0 +1,18 @@
+// AJAX = Asynchronous JavaScript And XML
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://carnes.cc/code/ajax_example.txt", true);
+    xhttp.send();
+  }
+  
+  /* 
+  Adding "https://cors-anywhere.herokuapp.com/" prevents the following error:
+  
+  XMLHttpRequest cannot load http://carnes.cc/code/ajax_example.txt. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'https://s.codepen.io' is therefore not allowed access.
+  */
