71eb10fe51665737bccbef745918dea62715f6b5
Hussein Salat <husseinsalat24@gmail.com>
2020-08-01 13:31:39 +0300

    Modified files round 102

--- DOM_Project/app.js
+++ DOM_Project/app.js
@@ -1,3 +1,6 @@
+/* QUESTION 3:Add a button element into the dom with a onclick event listener, which when clicked diSplays
+a pop up modal with the following text: */
+
 document.getElementById("popup-btn").addEventListener("click", function(){
 	document.getElementById("modal-area").style.display = "flex";
 })
@@ -5,3 +8,20 @@ document.getElementById("popup-btn").addEventListener("click", function(){
 document.querySelector(".modal-close").addEventListener('click', function(){
 	document.getElementById("modal-area").style.display = "none";
 })
+
+/* QUESTION 2:Using document.style.
+- Set the background Color of the section with id main to Blue,
+- padding to 0px
+- margin to 0px
+- display flex */
+
+// Selecting Element
+
+var lux = document.getElementById("main");
+
+// Applying styles on element
+
+lux.style.backgroundColor = "blue";
+lux.style.padding = "0px";
+lux.style.margin = "40px";
+lux.style.display = "flex";
