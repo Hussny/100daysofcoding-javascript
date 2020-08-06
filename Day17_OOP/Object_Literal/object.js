817fc98b62295c2e646fe1d7d45ad8ef632bc3f8
Hussein Salat <husseinsalat24@gmail.com>
2020-08-06 20:56:51 +0300

    Method Chaining

--- Day17_OOP/Object_Literal/object.js
+++ Day17_OOP/Object_Literal/object.js
@@ -32,7 +32,7 @@ console.log(userOne);
 console.log(userTwo);
 */
 // CLASS METHODS
-
+/*
 class User{
 constructor(email, name){
     this.email = email;
@@ -51,6 +51,31 @@ var userTwo = new User("xyz@gail.com", "xyz");
 
 userOne.login();
 userTwo.logout();
-
+*/
 
 // METHOD CHAINING 
+
+class User{
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, "just logged in");
+    }
+    logout(){
+        console.log(this.email, "just logged out");
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, "score is now", this.score);
+
+    }
+    }
+    
+    var userOne = new User("abc@gmail.com", "abd");
+    var userTwo = new User("xyz@gail.com", "xyz");
+    
+    userOne.login();
+    userTwo.logout();
