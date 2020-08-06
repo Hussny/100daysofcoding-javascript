760543d77a99a4daa53f0393231c545d40e0df04
Hussein Salat <husseinsalat24@gmail.com>
2020-08-06 17:31:32 +0300

    Object Literal
    Created new properties and methods

---
+++ Day17_OOP/Object_Literal/object.js
@@ -0,0 +1,12 @@
+var userOne = {
+    email : "husseinsalat24@gmail.com",
+    name: "Hussein",
+    login(){
+        console.log(this.email, "has logged in");
+    },
+    logout(){
+        console.log(this.email, "has logged out");
+    }
+};
+
+console.log(userOne.name);
