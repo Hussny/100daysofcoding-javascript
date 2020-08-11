59285a3ee41cee364d184765993ac6a0969a9541
Hussein Salat <husseinsalat24@gmail.com>
2020-08-11 19:47:12 +0300

    Added legth.js

---
+++ Day22_Arrays/length.js
@@ -0,0 +1,10 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.length;   // the length of fruits is 4
+
+console.log(fruits.length);
+
+/* 
+The length property of an array returns the
+ length of an array (the number of array
+     elements).
+     */
