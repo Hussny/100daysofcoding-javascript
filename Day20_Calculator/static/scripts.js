00dd0cf7cebe664a7b063624e15212932db5244a
Hussein Salat <husseinsalat24@gmail.com>
2020-08-20 21:16:13 +0300

    comment

--- Day20_Calculator/static/scripts.js
+++ Day20_Calculator/static/scripts.js
@@ -9,10 +9,10 @@ function load() {
   let p = document.querySelectorAll(" p")[0]; // obtain all of the <p> elements in the document
   let li = document.getElementsByTagName("li"); // obtain all of the <li> elements in the by tag name
   let clear = document.getElementsByClassName("clear")[0]; // obtain by class name clear which is C
-
-  /****************************************************************************************
- windows onload : tells windows what to do when it is loaded up
- *****************************************************************************************/
+  
+  /***********************
+ Looping through the array
+ ************************/
   for (let j = 0; j < li.length; j += 1) {
     if (li[j].innerHTML === "=") {
       li[j].addEventListener("click", calc(j));
@@ -20,9 +20,7 @@ function load() {
       li[j].addEventListener("click", value(j));
     }
   }
-  /****************************************************************************************
- windows onload : tells windows what to do when it is loaded up
- *****************************************************************************************/
+
   function value(j) {
     return function () {
       if (li[j].innerHTML === "÷") {
@@ -43,7 +41,7 @@ function load() {
   /*******************************
  Evaluate the code and executes it
  ********************************/
-  function calc(j) {
+function calc(j) {
     return function () {
       p.innerHTML = eval(p.innerHTML);
     };
