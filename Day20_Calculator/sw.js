c02376bed603fc9fe95b61a0435e103f02f48dcc
Hussein Salat <husseinsalat24@gmail.com>
2020-08-21 16:24:39 +0300

    Added app.js

--- Day20_Calculator/sw.js
+++ Day20_Calculator/sw.js
@@ -1,50 +1,43 @@
-const cacheName='v1';
+const cacheName = 'v2';
 
-const cacheAssets = [
-    'index.html',
-    'manifest.webmanifest',
-    'static/styles.css',
-    'static/scripts.js',
-    'app.js'
-]
-
-// CALL INSTALL EVENT 
+// Call Install Event
 self.addEventListener('install', e => {
-    console.log('Service Worker:Installed');
-
-    e.waitUntil(
-        caches
-        .open(cacheName)
-        .then(cache => {
-            console.log('Service Worker: Caching Files');
-            cache.addAll(cacheAssets);
-        })
-        .then(() => self.skipWaiting())
-    );
+  console.log('Service Worker: Installed');
 });
 
-// CALL ACTIVATE EVENT
+// Call Activate Event
 self.addEventListener('activate', e => {
-    console.log('Service Worker:Activated');
-    //REMOVE UNWANTED CACHES
-    e.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if(cache !==cacheName) {
-                        console.log('Service Worker: Clearing Old Caches');
-                        return caches.delete(cache);
-                    }
-                })
-            )
+  console.log('Service Worker: Activated');
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
         })
-    )
+      );
+    })
+  );
 });
 
-// SHOW CACHE FILES IN OFFLINE
+// Call Fetch Event
 self.addEventListener('fetch', e => {
-    console.log('Service Worker: Fetching');
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
-    );
+  console.log('Service Worker: Fetching');
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // Make copy/clone of response
+        const resClone = res.clone();
+        // Open cache
+        caches.open(cacheName).then(cache => {
+          // Add response to cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(err => caches.match(e.request).then(res => res))
+  );
 });
