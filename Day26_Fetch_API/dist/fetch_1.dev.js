78749958a38183cac39b43a0b97cd53d6e6b32a4
Hussein Salat <husseinsalat24@gmail.com>
2020-08-22 11:43:23 +0300

    fetch response

--- Day26_Fetch_API/dist/fetch_1.dev.js
+++ Day26_Fetch_API/dist/fetch_1.dev.js
@@ -1,5 +1,12 @@
 "use strict";
 
-fetch('https://regres.in/api/users').then(function (res) {
-  return console.log(res);
+fetch('https://regres.in/api/users') //.then (res => console.log(res))
+.then(function (res) {
+  return res.json();
+}).then(function (res) {
+  if (res.ok) {
+    console.log('Success');
+  } else {
+    console.log('Not Successful');
+  }
 });
--- Day26_Fetch_API/fetch_1.js
+++ Day26_Fetch_API/fetch_1.js
@@ -1,2 +1,10 @@
 fetch('https://regres.in/api/users')
-.then (res => console.log(res))
+//.then (res => console.log(res))
+.then(res => res.json())
+.then (res=> {
+    if(res.ok){
+        console.log('Success');
+    } else{
+        console.log('Not Successful');
+    }
+})
