5fedf107d721a1bb54a635f5926c3da201c18f8c
Hussein Salat <husseinsalat24@gmail.com>
2020-08-22 12:34:58 +0300

    fetch(url)

--- Day26_Fetch_API/fetch_1.js
+++ Day26_Fetch_API/fetch_1.js
@@ -1,2 +1,10 @@
 const ul = document.getElementById('authors'); // Get the list where we will place our authors
 const url = 'https://randomuser.me/api/?results=10'; // Get 10 random users
+
+fetch(url)
+  .then(function(data) {
+    // Here you get the data to modify as you please
+    })
+  .catch(function(error) {
+    // If there is any error you will catch them here
+  });
