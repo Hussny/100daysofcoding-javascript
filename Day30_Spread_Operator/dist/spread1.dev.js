eee2fc20b99fecfdb72b6536c35bea2d18b5d45b
Hussein Salat <husseinsalat24@gmail.com>
2020-08-26 20:12:32 +0300

    comment on  spread1.js

---
+++ Day30_Spread_Operator/dist/spread1.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var odd = [1, 3, 5];
+var combine = [9, 4, 7].concat(odd); // The spread operator unpacks the elements of the odd Array
+
+console.log(combine);
--- Day30_Spread_Operator/spread1.js
+++ Day30_Spread_Operator/spread1.js
@@ -1,4 +1,5 @@
 const odd = [1,3,5];
-const combine = [9,4,7, ...odd];
+const combine = [9,4,7, ...odd]; // The spread operator unpacks the elements of the odd Array
 
 console.log(combine);
+
