65087b146757c7b2e5d229df76385f5306f409b2
Hussny <husseinsalat24@gmail.com>
2020-09-01 18:15:49 +0300

    using spread operator comments

--- Day31_UseCases/spread1.js
+++ Day31_UseCases/spread1.js
@@ -1,4 +1,6 @@
+// Adding the element of an existing array into a new array
+
 var mid_fielders = ["Draxler", "Herrera", "Gueye"];
-var strikers = ["Cavani", "Chop Moting", ...mid_fielders];
+var strikers = ["Cavani", "Chop Moting", ...mid_fielders]; // using the ... spread operator
 
 console.log(strikers);
