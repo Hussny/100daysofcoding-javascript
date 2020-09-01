2edce745911b8832f854d48c37d4f665af8d12e8
Hussny <husseinsalat24@gmail.com>
2020-09-01 18:37:31 +0300

    copying arrays using spread operator

--- Day31_UseCases/spread3.js
+++ Day31_UseCases/spread3.js
@@ -1,3 +1,8 @@
 // copy arrays
 
-var defenders = ["thiago", "kimpembe", "kurzawa"];
+var defenders = [1, 2, 3];
+var centerbacks = [...defenders];   // like defenders.slice()
+centerbacks.push(4);
+
+console.log(defenders);
+console.log(centerbacks);
